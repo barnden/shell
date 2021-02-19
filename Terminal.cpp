26f67fef3bdf49d5df579aff9a0eb431feb70729
barnden <brandon@barnden.dev>
2021-02-19 11:33:05 -0600

    fix duplicate prompt strings from SIGWINCH

--- Terminal.cpp
+++ Terminal.cpp
@@ -8,6 +8,8 @@
 #include "Terminal.h"
 
 namespace BShell {
+volatile bool g_fsigint = false;
+
 void rl$reset() {
     rl_crlf();
     rl_reset_line_state();
@@ -20,24 +22,27 @@ void handler$posix_sig(int) {
     // Main objective of this handler is to stop the shell process
     // from exiting upon SIGINT/CTRL+C.
 
-    // This function is empty so the signal doesn't get processed
-    // and exit out of the process, however, we don't want to
-    // pass SIG_IGN to ignore the signal, as we need to execute
-    // the handler$readline_sig() function below. With SIG_IGN
-    // that function will not be called.
+    // This function does not reset the prompt string due to unsafe
+    // malloc from GNU readline within signal handler. Use
+    // rl_hook_func_t to hook into readline's signal handler to
+    // reset the prompt string. Use flag to determine when we have
+    // received SIGINT, because rl_hook_func_t does not tell us
+    // which signal has been called.
+
+    g_fsigint = true;
 }
 
 void handler$readline_sig(void) {
     // This function handles when readline is interrupted by a signal
-    // In the case of CTRL+C, the SIGINT sends ETX which the terminal
-    // displays as ^C. This function clears the line buffer and goes
-    // to a new line, so the user can continue to use the shell
-    // without garbage in the terminal display or line buffer.
 
-    // I'm not entirely sure how to check to see if it was SIGINT
-    // that caused this handler function to run. From the readline
-    // docs it says that rl_hook_func_t takes no parameters.
+    if (g_fsigint) {
+        // In the case of CTRL+C, the SIGINT sends ETX which the terminal
+        // displays as ^C. This function clears the line buffer and goes
+        // to a new line, so the user can continue to use the shell
+        // without garbage in the terminal display or line buffer.
 
-    rl$reset();
+        rl$reset();
+        g_fsigint = false;
+    }
 }
 }
--- Terminal.h
+++ Terminal.h
@@ -1,6 +1,7 @@
 #pragma once
 
 namespace BShell {
+extern volatile bool g_fsigint;
 void rl$reset();
 void handler$readline_sig(void);
 void handler$posix_sig(int);
