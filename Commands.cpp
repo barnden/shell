769f6436101e75eb772ce5ca1b0929cbf0208961
barnden <brandon@barnden.dev>
2021-02-24 11:14:18 -0600

    Commands: Use argv as std::string, use unique_ptr

--- Commands.cpp
+++ Commands.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 #include <string.h>
 #include <unistd.h>
@@ -8,13 +9,21 @@
 
 namespace BShell {
 void command$cd(Expression* expr) {
+    auto args = std::vector<std::string> {};
     auto argv = std::vector<char*> {};
+
     auto sticky = false;
 
     for (auto& child : expr->children)
-        handle$argv_strings(argv, sticky, child->token);
+        handle$argv_strings(args, sticky, child->token);
 
-    auto* dir = argv.size() ? argv[0] : nullptr;;
+    std::transform(
+        args.begin(), args.end(),
+        std::back_inserter(argv),
+        [](const std::string& str) { return const_cast<char*>(str.c_str()); }
+    );
+
+    auto* dir = argv.size() ? argv[0] : nullptr;
     auto cwd = get$cwd();
     auto stat = 0;
 
@@ -22,18 +31,15 @@ void command$cd(Expression* expr) {
         stat = chdir(get$home().c_str());
     else if (dir[0] == '~') {
         auto home = get$home();
-        auto* path = new char [strlen(dir) + home.size()];
+        auto path = std::make_unique<char>(strlen(dir) + home.size());
 
-        strcpy(path, home.c_str());
-        strcpy(path + home.size(), dir + 1);
+        strcpy(path.get(), home.c_str());
+        strcpy(path.get() + home.size(), dir + 1);
 
-        stat = chdir(path);
-
-        delete[] path;
+        stat = chdir(path.get());
     } else if (strcmp(dir, "-") == 0) {
         if (g_prev_wd.size()) {
             stat = chdir(g_prev_wd.c_str());
-            std::cout << g_prev_wd << '\n';
         } else std::cerr << "g_prev_wd not set\n";
     } else stat = chdir(dir);
 
@@ -42,8 +48,5 @@ void command$cd(Expression* expr) {
         std::cerr << "Failed to change directory\n";
     else
         g_prev_wd = cwd;
-
-    for (auto& arg : argv)
-        delete[] arg;
 }
 }
