733a1b96451cf4f766a9d18ec7098e4458061231
barnden <brandon@barnden.dev>
2021-03-05 18:56:25 -0600

    Terminal: Tab completion
    Very basic tab completion, it only grabs the first file.

--- Terminal.cpp
+++ Terminal.cpp
@@ -1,5 +1,6 @@
 #include <string>
 
+#include <filesystem>
 #include <iostream>
 #include <vector>
 
@@ -9,6 +10,7 @@
 #include <unistd.h>
 
 #include "PromptString.h"
+#include "Shell.h"
 #include "Terminal.h"
 #include "Tokenizer.h"
 
@@ -17,8 +19,8 @@ termios g_term, g_oterm;
 std::vector<std::string> g_history = std::vector<std::string> {};
 
 void handle$sigint(int) {
-    // We do not want to completely SIG_IGN SIGINT, however,
-    // we also do not want the terminal to exit on SIGINT.
+    // We do not want the shell to exit on SIGINT
+    // but we also do not want to SIG_IGN SIGINT.
 }
 
 std::vector<std::string> g_token_colors = {
@@ -39,7 +41,7 @@ std::vector<std::string> g_token_colors = {
     "\x1b[0m",  // WhiteSpace
 };
 
-std::string color_tokens(std::string input) {
+std::string line$color(std::string input) {
     auto str = std::string {};
     auto tokens = Tokenizer(input, true).tokens();
 
@@ -49,12 +51,45 @@ std::string color_tokens(std::string input) {
     return str + "\x1b[0m";
 }
 
+void line$reprint(const std::string& prompt, const std::string& line_buf, int x) {
+    std::cout
+        << "\x1b[2K\r"
+        << prompt << line$color(line_buf);
+
+    if (x > 0)
+        std::cout << "\x1b[1G\x1b[" << std::to_string(x) << "C";
+}
+
+void history$prev(int* cursor, bool& lup, const std::string& prompt, std::string& line_buf) {
+    if (g_history.size() && cursor[1] < g_history.size()) {
+        lup = true;
+
+        line_buf = *(g_history.rbegin() + cursor[1]++);
+        cursor[0] = line_buf.size();
+
+        line$reprint(prompt, line_buf, cursor[0] + prompt.size());
+    }
+}
+
+void history$next(int* cursor, bool& lup, const std::string& prompt, std::string& line_buf) {
+    if (lup) {
+        lup = false;
+        cursor[1]--;
+    }
+
+    line_buf = cursor[1] ? *(g_history.rbegin() + --cursor[1]) : "";
+    cursor[0] = line_buf.size();
+
+    line$reprint(prompt, line_buf, cursor[0] + prompt.size());
+}
+
 std::string get$input(std::string prompt) {
     auto c = char {};
     auto line_buf = std::string {};
-
+    auto lup = false;
     int cursor[2] = {0, 0};
 
+    // Print out prompt before starting loop
     std::cout << "\x1b[2K\x1b[1G" << prompt;
 
     while (read(STDIN_FILENO, &c, 1) == 1) {
@@ -67,6 +102,7 @@ std::string get$input(std::string prompt) {
         
         if (c == '\r') {
             std::cout << '\n';
+
             return line_buf;
         }
 
@@ -78,16 +114,50 @@ std::string get$input(std::string prompt) {
             line_buf = "";
 
             continue;
-        } else if (c == g_term.c_cc[VERASE]) {
+        }
+
+        if (c == g_term.c_cc[VERASE]) {
             // Backspace
-            if (line_buf.size()) {
+            if (line_buf.size() && cursor[0] > 0){
                 line_buf.erase(--cursor[0], 1);
-                std::cout
-                    << "\x1b[2K\r"
-                    << prompt << color_tokens(line_buf)
-                    << "\x1b[1G\x1b[" << (cursor[0] + prompt.size()) << "C";
+                line$reprint(prompt, line_buf, cursor[0] + prompt.size());
             }
 
+            continue;
+        }
+
+        if (c == '\t') {
+            auto last = line_buf;
+            auto ac_index = 0;
+            std::vector<std::filesystem::path> files = {std::filesystem::directory_iterator(get$cwd()), {}};
+            std::vector<std::string> filenames;
+
+            for (auto& f : files) {
+                auto fname = f.string();
+                auto find = fname.find_last_of('/');
+
+                if (find != std::string::npos)
+                    fname = fname.substr(find + 1);
+
+                filenames.push_back(fname);
+            }
+
+            if ((ac_index = line_buf.find_last_of(' ')) != std::string::npos)
+                last = line_buf.substr(ac_index + 1);
+
+            filenames.erase(
+                std::remove_if(filenames.begin(), filenames.end(),
+                    [=](std::string str) {
+                        return str.size() < last.size() || str.substr(0, last.size()) != last;
+                    }
+                ), filenames.end()
+            );
+
+            if (filenames.size()) {
+                line_buf += filenames[0].substr(last.size());
+                cursor[0] = line_buf.size();
+                line$reprint(prompt, line_buf, cursor[0] + prompt.size());
+            }
             continue;
         } else if (c == '\x1b') {
             // Special characters
@@ -98,30 +168,28 @@ std::string get$input(std::string prompt) {
                 inp[count] = '\0';
 
                 if (strcmp(inp, "[A") == 0) {
-                    // UP
-                    if (cursor[1] < g_history.size())
-                        ++cursor[1];
+                    // ARROW UP
+                    history$prev(cursor, lup, prompt, line_buf);
                 } else if (strcmp(inp, "[B") == 0) {
-                    // DOWN
-                    if (cursor[1] > 0)
-                        --cursor[1];
+                    // ARROW DOWN
+                    history$next(cursor, lup, prompt, line_buf);
                 } else if (strcmp(inp, "[C") == 0) {
+                    // ARROW RIGHT
                     if (cursor[0] < line_buf.size()) {
                         ++cursor[0];
                         std::cout << "\x1b[1C";
                     }
                 } else if (strcmp(inp, "[D") == 0) {
+                    // ARROW LEFT
                     if (cursor[0] > 0) {
                         --cursor[0];
                         std::cout << "\x1b[1D";
                     }
                 } else if (strcmp(inp, "[3~") == 0) {
+                    // DEL
                     if (line_buf.size()) {
                         line_buf.erase(cursor[0], 1);
-                        std::cout
-                            << "\x1b[2K\r"
-                            << prompt << color_tokens(line_buf)
-                            << "\x1b[1G\x1b[" << (cursor[0] + prompt.size()) << "C";
+                        line$reprint(prompt, line_buf, cursor[0] + prompt.size());
                     }
                 } else if (strcmp(inp, "[5~") == 0) {
                     // PGUP
@@ -149,20 +217,14 @@ std::string get$input(std::string prompt) {
             continue;
         }
 
-        if (cursor[0] < line_buf.size()) {
-            if (line_buf.size())
-                line_buf.insert(cursor[0], 1, c);
-            else
-                line_buf = std::string { c };
+        cursor[1] = 0;
 
-            std::cout
-                << "\x1b[2K\r" << prompt << color_tokens(line_buf)
-                << "\x1b[1G\x1b[" << (++cursor[0] + prompt.size()) << "C";
-        } else {
-            ++cursor[0];
+        if (cursor[0] < line_buf.size())
+            line_buf.insert(cursor[0], 1, c);
+        else
             line_buf += c;
-            std::cout << "\x1b[1K\x1b[1G\r" << prompt << color_tokens(line_buf);
-        }
+
+        line$reprint(prompt, line_buf, ++cursor[0] + prompt.size());
     }
 }
 
