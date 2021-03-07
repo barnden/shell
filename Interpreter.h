31c8aa27503fd08d8220805eebe585bf258a09d3
barnden <brandon@barnden.dev>
2021-03-06 20:26:39 -0600

    Interpreter: Remove old function stubs in header.
    Before, the System functions were defined inside Interpreter.
    
    This is no longer the case and these function stubs should not be here.

--- Interpreter.h
+++ Interpreter.h
@@ -21,14 +21,6 @@ enum KEYWORD {
     CD
 };
 
-std::string get$cwd();
-std::string get$home();
-std::string get$username();
-std::string get$hostname();
-std::string get$pname(pid_t);
-std::string get$pname(const std::shared_ptr<Expression>&);
-std::string get$executable_path(std::string);
-
 void set$cwd(std::string);
 void erase_dead_children();
 
