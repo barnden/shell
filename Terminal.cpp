efc108bf4f54fce14832365901bfe2b69bdb3d55
barnden <brandon@barnden.dev>
2021-04-23 14:34:22 -0500

    Terminal: Use new formatting

--- Terminal.cpp
+++ Terminal.cpp
@@ -22,21 +22,11 @@ termios g_term, g_oterm;
 std::vector<std::string> g_history = std::vector<std::string> {};
 
 std::unordered_map<TokenType, std::string> g_token_colors = {
-    { NullToken, "\x1b[0m" },
-    { String, "\x1b[0m" },
-    { Equal, "\x1b[0m" },
-    { Executable, "\x1b[34m" },
-    { Background, "\x1b[31m" },
-    { Sequential, "\x1b[31m" },
-    { SequentialIf, "\x1b[32m" },
-    { RedirectPipe, "\x1b[32m" },
-    { RedirectOut, "\x1b[32m" },
-    { RedirectIn, "\x1b[32m" },
-    { Key, "\x1b[34m" },
-    { Eval, "\x1b[36m" },
-    { StickyRight, "\x1b[0m" },
-    { StickyLeft, "\x1b[0m" },
-    { WhiteSpace, "\x1b[0m" },
+    { NullToken, "\x1b[0m" },     { String, "\x1b[0m" },        { Equal, "\x1b[0m" },
+    { Executable, "\x1b[34m" },   { Background, "\x1b[31m" },   { Sequential, "\x1b[31m" },
+    { SequentialIf, "\x1b[32m" }, { RedirectPipe, "\x1b[32m" }, { RedirectOut, "\x1b[32m" },
+    { RedirectIn, "\x1b[32m" },   { Key, "\x1b[34m" },          { Eval, "\x1b[36m" },
+    { StickyRight, "\x1b[0m" },   { StickyLeft, "\x1b[0m" },    { WhiteSpace, "\x1b[0m" },
 };
 
 void handle$sigint(int) {
@@ -54,42 +44,39 @@ std::string line$color(std::string input) {
     return str + "\x1b[0m";
 }
 
-void line$reprint(std::string const& prompt, std::string const& line_buf, int x) {
-    std::cout
-        << "\x1b[2K\r"
-        << prompt
-        << line$color(line_buf);
+void line$reprint(std::string const& prompt, std::string const& input, int x) {
+    std::cout << "\x1b[2K\r" << prompt << line$color(input);
 
     if (x > 0)
         std::cout << "\x1b[1G\x1b[" << std::to_string(x) << "C";
 }
 
-void history$prev(int* cursor, bool& lup, std::string const& prompt, std::string& line_buf) {
-    if (!g_history.size() || cursor[1] >= g_history.size())
+void history$prev(int& x, int& y, bool& lup, std::string const& prompt, std::string& input) {
+    if (!g_history.size() || y >= g_history.size())
         return;
 
     lup = true;
 
-    line_buf = *(g_history.rbegin() + cursor[1]++);
-    cursor[0] = line_buf.size();
+    input = *(g_history.rbegin() + y++);
+    x = input.size();
 
-    line$reprint(prompt, line_buf, cursor[0] + prompt.size());
+    line$reprint(prompt, input, x + prompt.size());
 }
 
-void history$next(int* cursor, bool& lup, std::string const& prompt, std::string& line_buf) {
+void history$next(int& x, int& y, bool& lup, std::string const& prompt, std::string& input) {
     if (lup) {
         lup = false;
-        cursor[1]--;
+        y--;
     }
 
-    line_buf = cursor[1] ? *(g_history.rbegin() + --cursor[1]) : "";
-    cursor[0] = line_buf.size();
+    input = y ? *(g_history.rbegin() + --y) : "";
+    x = input.size();
 
-    line$reprint(prompt, line_buf, cursor[0] + prompt.size());
+    line$reprint(prompt, input, x + prompt.size());
 }
 
-void terminal$autocomplete(int* cursor, std::string const& prompt, std::string& line_buf) {
-    auto last = line_buf;
+void terminal$autocomplete(int& x, std::string const& prompt, std::string& input, int c) {
+    auto last = input;
     auto find = size_t {};
     auto files = std::vector<fs::path> { fs::directory_iterator(get$cwd()), {} };
     auto filenames = std::vector<std::string> {};
@@ -103,138 +90,182 @@ void terminal$autocomplete(int* cursor, std::string const& prompt, std::string& 
         filenames.push_back(path);
     }
 
-    if ((find = line_buf.find_last_of(' ')) != std::string::npos)
-        last = line_buf.substr(find + 1);
+    if ((find = input.find_last_of(' ')) != std::string::npos)
+        last = input.substr(find + 1);
 
-    filenames.erase(
-        std::remove_if(filenames.begin(), filenames.end(),
-            [=](std::string const& str) {
-                return str.size() < last.size() || str.substr(0, last.size()) != last;
-            }),
-        filenames.end());
+    filenames.erase(std::remove_if(filenames.begin(), filenames.end(),
+                                   [=](std::string const& str) {
+                                       return str.size() < last.size()
+                                              || str.substr(0, last.size()) != last;
+                                   }),
+                    filenames.end());
 
     if (filenames.size()) {
-        line_buf += filenames[0].substr(last.size());
-        cursor[0] = line_buf.size();
-        line$reprint(prompt, line_buf, cursor[0] + prompt.size());
+        input += filenames[0].substr(last.size());
+        x = input.size();
+        line$reprint(prompt, input, x + prompt.size());
+    }
+}
+
+void terminal$ansi_handler(std::string const& prompt, int& x, int& y, std::string& input,
+                           bool& lup) {
+    char ansi[4];
+    auto count = read(STDIN_FILENO, &ansi, 3);
+
+    if (count == 0)
+        return;
+
+    if (count <= 0) {
+        perror("read()");
+        exit(1);
+    }
+
+    ansi[count] = '\0';
+
+    if (strcmp(ansi, "[A") == 0) {
+        // ARROW UP
+        history$prev(x, y, lup, prompt, input);
+        return;
+    }
+
+    if (strcmp(ansi, "[B") == 0) {
+        // ARROW DOWN
+        history$next(x, y, lup, prompt, input);
+        return;
+    }
+
+    if (strcmp(ansi, "[C") == 0) {
+        // ARROW RIGHT
+        if (x < input.size()) {
+            ++x;
+            std::cout << "\x1b[1C";
+        }
+        return;
+    }
+
+    if (strcmp(ansi, "[D") == 0) {
+        // ARROW LEFT
+        if (x > 0) {
+            --x;
+            std::cout << "\x1b[1D";
+        }
+        return;
+    }
+
+    if (strcmp(ansi, "[3~") == 0) {
+        // DEL
+        if (input.size()) {
+            input.erase(x, 1);
+            line$reprint(prompt, input, x + prompt.size());
+        }
+        return;
+    }
+
+    if (strcmp(ansi, "[5~") == 0) {
+        // PGUP
+        if (y + 5 < g_history.size())
+            y += 5;
+        return;
+    }
+
+    if (strcmp(ansi, "[6~") == 0) {
+        // PGDN
+        history$prev(x, y, lup, prompt, input);
+        return;
+    }
+
+    if (strcmp(ansi, "[H") == 0) {
+        // HOME
+        if (x)
+            std::cout << "\x1b[" << x << "D";
+
+        x = 0;
+        return;
+    }
+
+    if (strcmp(ansi, "[F") == 0) {
+        // END
+        if (x < input.size()) {
+            std::cout << "\x1b[" << (input.size() - x) << "C";
+            x = input.size();
+        }
+        return;
     }
 }
 
 std::string get$input(std::string const& prompt) {
     auto c = char {};
-    auto line_buf = std::string {};
+    auto input = std::string {};
+    auto x = 0, y = 0;
+    auto tabc = 0;
     auto lup = false;
-    int cursor[2] = { 0, 0 };
 
     terminal$control();
 
-    // Print out prompt before starting loop
+    // Print prompt string before starting loop
     std::cout << "\x1b[2K\x1b[1G" << prompt;
 
     while (read(STDIN_FILENO, &c, 1) == 1) {
         // termios::c_cc is runtime; no switches ;(
         if (c == g_term.c_cc[VEOF]) {
-            // CTRL+D (EOF to STDIN)
+            // CTRL+D (EOF)
             std::cout << "^D\x1b[1G\nbrandon shell exited\n\x1b[2K\x1b[1G";
             return "\x1b[EOF";
         }
 
         if (c == g_term.c_cc[VINTR]) {
             // CTRL+C (SIGINT)
-            std::cout << "^C";
-            std::cout << "\n\x1b[1G" << prompt;
-            cursor[0] = 0;
-            line_buf = "";
+            std::cout << "^C\n\x1b[1G" << prompt;
+
+            x = 0;
+            input = "";
 
             continue;
         }
 
         if (c == g_term.c_cc[VERASE]) {
             // Backspace
-            if (line_buf.size() && cursor[0] > 0) {
-                line_buf.erase(--cursor[0], 1);
-                line$reprint(prompt, line_buf, cursor[0] + prompt.size());
+            if (input.size() && x) {
+                input.erase(--x, 1);
+                line$reprint(prompt, input, x + prompt.size());
             }
 
             continue;
         }
 
+        if (c == 23) {
+            // CTRL+Backspace
+        }
+
         if (c == '\r') {
+            // Return
             std::cout << "\n\x1b[2K\x1b[1G";
             terminal$restore();
 
-            return line_buf;
+            return input;
         }
 
         if (c == '\t') {
-            terminal$autocomplete(cursor, prompt, line_buf);
+            // Tab
+            terminal$autocomplete(x, prompt, input, ++tabc);
             continue;
         }
 
         if (c == '\x1b') {
-            // Special characters
-            char inp[4];
-            auto count = 0;
-
-            if ((count = read(STDIN_FILENO, &inp, 3)) > 0) {
-                inp[count] = '\0';
-
-                if (strcmp(inp, "[A") == 0) {
-                    // ARROW UP
-                    history$prev(cursor, lup, prompt, line_buf);
-                } else if (strcmp(inp, "[B") == 0) {
-                    // ARROW DOWN
-                    history$next(cursor, lup, prompt, line_buf);
-                } else if (strcmp(inp, "[C") == 0) {
-                    // ARROW RIGHT
-                    if (cursor[0] < line_buf.size()) {
-                        ++cursor[0];
-                        std::cout << "\x1b[1C";
-                    }
-                } else if (strcmp(inp, "[D") == 0) {
-                    // ARROW LEFT
-                    if (cursor[0] > 0) {
-                        --cursor[0];
-                        std::cout << "\x1b[1D";
-                    }
-                } else if (strcmp(inp, "[3~") == 0) {
-                    // DEL
-                    if (line_buf.size()) {
-                        line_buf.erase(cursor[0], 1);
-                        line$reprint(prompt, line_buf, cursor[0] + prompt.size());
-                    }
-                } else if (strcmp(inp, "[5~") == 0) {
-                    // PGUP
-                    if (cursor[1] + 5 < g_history.size())
-                        cursor[1] += 5;
-                } else if (strcmp(inp, "[6~") == 0) {
-                    // PGDN
-                    history$prev(cursor, lup, prompt, line_buf);
-                } else if (strcmp(inp, "[H") == 0) {
-                    // HOME
-                    if (cursor[0])
-                        std::cout << "\x1b[" << cursor[0] << "D";
-
-                    cursor[0] = 0;
-                } else if (strcmp(inp, "[F") == 0) {
-                    // END
-                    if (cursor[0] < line_buf.size()) {
-                        std::cout << "\x1b[" << (line_buf.size() - cursor[0]) << "C";
-                        cursor[0] = line_buf.size();
-                    }
-                }
-            }
-
+            // ANSI control characters
+            terminal$ansi_handler(prompt, x, y, input, lup);
             continue;
         }
 
-        line_buf.insert(cursor[0], 1, c);
+        input.insert(x, 1, c);
 
-        cursor[0]++;
-        cursor[1] = 0;
+        x++;
+        y = 0;
 
-        line$reprint(prompt, line_buf, cursor[0] + prompt.size());
+        line$reprint(prompt, input, x + prompt.size());
     }
+
+    return input;
 }
 
 void terminal$control() {
